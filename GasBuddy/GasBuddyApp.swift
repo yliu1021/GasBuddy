72d2909d8cfee7569fd26e4c0ce290319a547cb9
Yuhan Liu <yliu1021@outlook.com>
2021-11-30 07:54:29 -0800

    Initial Commit

---
+++ GasBuddy/GasBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasBuddyApp.swift
+//  GasBuddy
+//
+//  Created by Yuhan Liu on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct GasBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
